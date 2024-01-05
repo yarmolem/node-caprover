be0a5b6f26a19e95a6facd90ae54d20a6af2897c
Cristian Gonzalez <kazragore96@gmail.com>
2024-01-05 11:59:19 -0500

    feat: Agregado dotenv

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
+import 'dotenv/config'
 import express from 'express'
 
 const app = express()
+
+const PORT = process.env.PORT
 
 app.get('/', (req, res) => {
   res.json({ ok: true, message: 'Hello World!' })
 })
 
-app.listen(3001, () => {
-  console.log('Example app listening on port 3000!')
+app.listen(PORT, () => {
+  console.log('Example app listening on ' + 'http://localhost:' + PORT + '!')
 })
