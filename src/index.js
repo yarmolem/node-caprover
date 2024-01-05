14905d4f1bfe4c2a2ac4e6b8e70c3e357515884f
Cristian Gonzalez <kazragore96@gmail.com>
2024-01-05 11:49:53 -0500

    feat: Modificado puerto

--- src/index.js
+++ src/index.js
@@ -6,6 +6,6 @@ app.get('/', (req, res) => {
   res.json({ ok: true, message: 'Hello World!' })
 })
 
-app.listen(3000, () => {
+app.listen(3001, () => {
   console.log('Example app listening on port 3000!')
 })
